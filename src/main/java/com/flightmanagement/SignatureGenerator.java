ab9e297a9a6e62f8068e19890d2dea70ce96ee13
Mantu Kumar <mantumeta15@mail@gmail.com>
2026-04-30 12:37:29 +0530

    Removed hardcoded secret and updated gitignore

--- src/main/java/com/flightmanagement/SignatureGenerator.java
+++ src/main/java/com/flightmanagement/SignatureGenerator.java
@@ -8,7 +8,7 @@ public class SignatureGenerator {
     public static void main(String[] args) throws Exception {
         String orderId = "order_Sg3LXjwcErqA79";
         String paymentId = "pay_test_123456";
-        String secret = "nkRKcQAtzZr14yAx7ZiSQ6mJ";
+        String secret = System.getenv("RAZORPAY_SECRET");
 
         // Signature generate karo
         String data = orderId + "|" + paymentId;
